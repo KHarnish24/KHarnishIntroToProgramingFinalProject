896c1d89ea35c0b94139dbe820ffa962cd99831e
KHarnish24 <KHarnish24@users.noreply.github.com>
2022-12-19 17:03:46 -0800

    added fog

--- artifact_handler.py
+++ artifact_handler.py
@@ -1,6 +1,8 @@
 import pygame as pg
+import sys
 from settings import *
 from main import *
+
 
 class ArtifactHandler:
 
@@ -9,7 +11,13 @@ class ArtifactHandler:
             self.points = 0
             self.points_needed = len(artifacts)
             self.collected = False
+            self.won = False
+            self.lost = False
             self.game = game
+            self.w_text = pg.image.load("resources/sprites/text_sprites/mission_complete.png").convert_alpha()
+            self.text_1 = pg.image.load("resources/sprites/text_sprites/mission_complete.png").convert_alpha()
+            self.text = 0
+            
 
     def gather(self):
         self.points += 1
@@ -18,13 +26,50 @@ class ArtifactHandler:
         if(self.points >= self.points_needed):
             self.collected = True
 
+    def text(self):
+        cur_time = pg.time.get_ticks()
+        start_time = 0
+        if self.text == 0:
+            start_time = pg.time.get_ticks()
+            self.text = 1
+        elif self.text ==1 and cur_time - 5000 > start_time:
+            start_time = pg.time.get_ticks()
+            self.text = 2
+        elif self.text == 2 and cur_time - 5000 > start_time:
+            start_time = 3
+        else:
+            pass
+            
+
+
     def update(self):
+        cur_time = pg.time.get_ticks()
+        start_time = 0
         if self.collected == False:
             self.check_collected()
         else:
             if self.game.player.map_pos == (7,33) or self.game.player.map_pos == (8,33):
-                print('you win')
-                pass
+                start_time = pg.time.get_ticks()
+                self.won = True
+
+            if self.won == True:
+
+                if cur_time - 5000 > start_time:
+                    pg.quit()
+                    sys.exit()
+                
+
+
+            
+                
+                
+    def draw(self):
+        if self.won == True and self.lost == False:
+            self.game.screen.blit(self.w_text, (350, 100))
+        if self.text == 1
+
+            
+        
 
         
             
